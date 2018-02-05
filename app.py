aaefd9a2f08f99314807b6d07e8ea25cd6ad9834
abc <a@b.c>
2018-02-05 13:32:47 +0530

    fucki

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ def getName():
     return input("What is your name?\n")
 
 def main():
-    print(greeter(getName))
+    rnbl = greeter(getName)
+    print(rnbl)
+    return rnbl
 
 if __name__ == "__main__":
     main()
